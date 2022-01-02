d968edc4037db66f3c1b841096e537fa1e30fa36
CU-0xff <frank@cu-0xff.de>
2022-01-02 14:22:56 +0100

    TSconfig fixed so it allows import now

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
-//import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
 
+import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
 
 class App {
     constructor() {
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
         clean: true,
     },
     resolve: {
+        modules: [__dirname , 'node_modules'],
         extensions: [".tsx", ".ts", ".js"],
     },
     devServer: {

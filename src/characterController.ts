b49cc53a95c23d894dfbcebc9d24dcaba08ad817
CU-0xff <frank@cu-0xff.de>
2022-01-03 14:29:22 +0100

    Following camera

--- src/characterController.ts
+++ src/characterController.ts
@@ -1,12 +1,16 @@
-import { TransformNode, Mesh, Scene, ShadowGenerator, ArcRotateCamera, Vector3 } from "@babylonjs/core";
+import { TransformNode, Mesh, Scene, ShadowGenerator, ArcRotateCamera, Vector3, UniversalCamera } from "@babylonjs/core";
+//import { PlayerInput } from "./inputController";
 
 export class Player extends TransformNode {
-    public camera;
+    public camera : UniversalCamera;
     public scene: Scene;
-    private _input;
-
+    private _input ;//: PlayerInput;
+    private _camRoot: TransformNode;
+    private _yTilt : TransformNode;
     public mesh: Mesh;
 
+    private static readonly ORIGINAL_TILT: Vector3 = new Vector3(0.5934119456780721, 0, 0);
+    
     constructor(assets, scene: Scene, shadowGenerator: ShadowGenerator, input?) {
         super("player", scene);
         this.scene = scene;
@@ -19,6 +23,31 @@ export class Player extends TransformNode {
     }
 
     private _setupPlayerCamera() {
-        var camera4 = new ArcRotateCamera("arc", -Math.PI/2, Math.PI/2, 40, new Vector3(0,3,0), this.scene);
+        //root camera parent that handles positioning of the camera to follow the player
+        this._camRoot = new TransformNode("root");
+        this._camRoot.position = new Vector3(0, 0, 0); //initialized at (0,0,0)
+        //to face the player from behind (180 degrees)
+        this._camRoot.rotation = new Vector3(0, Math.PI, 0);
+    
+        //rotations along the x-axis (up/down tilting)
+        let yTilt = new TransformNode("ytilt");
+        //adjustments to camera view to point down at our player
+        yTilt.rotation = Player.ORIGINAL_TILT;
+        this._yTilt = yTilt;
+        yTilt.parent = this._camRoot;
+    
+        //our actual camera that's pointing at our root's position
+        this.camera = new UniversalCamera("cam", new Vector3(0, 0, -30), this.scene);
+        this.camera.lockedTarget = this._camRoot.position;
+        this.camera.fov = 0.47350045992678597;
+        this.camera.parent = yTilt;
+    
+        this.scene.activeCamera = this.camera;
+        return this.camera;
+    }
+    
+    private _updateCamera(): void {
+        let centerPlayer = this.mesh.position.y + 2;
+        this._camRoot.position = Vector3.Lerp(this._camRoot.position, new Vector3(this.mesh.position.x, centerPlayer, this.mesh.position.z), 0.4);
     }
 }
